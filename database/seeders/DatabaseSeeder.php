755f978f936e45b60d0763bccff0ec1da19c219d
Andras Nagy <contact@andras.me>
2021-07-28 00:59:37 +0200

    add data with seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Schedule;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Schedule::create([
+            'scheduled_from' => '2021-06-01',
+            'scheduled_till' => '2021-11-30',
+            'day_name' => 'Thursday',
+            'active_week' => 'all',
+            'begins_at' => '16:00:00',
+            'ends_at' => '20:00:00',
+        ]);
     }
 }
