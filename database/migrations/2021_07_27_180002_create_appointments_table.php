ae4accdc633e438af91ef9dd4e3dc85e3b0eb2a9
Andras Nagy <contact@andras.me>
2021-07-27 22:44:01 +0200

    crate tables for appointments

---
+++ database/migrations/2021_07_27_180002_create_appointments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('patient_id')
+                  ->constrained('patients')
+                  ->onDelete('cascade');
+            $table->datetime('begins_at');
+            $table->datetime('ends_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
