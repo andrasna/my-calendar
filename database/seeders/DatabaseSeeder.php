60bf4951979a9cae60a236b72d16ed411cd58205
Andras Nagy <contact@andras.me>
2021-07-28 17:33:29 +0200

    add data with seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,37 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Schedule::create([
+            'scheduled_from' => '2021-06-08',
+            'active_week' => 'one',
+            'begins_at' => '08:00:00',
+            'ends_at' => '10:00:00',
+        ]);
+
+        Schedule::create([
+            'scheduled_from' => '2021-01-01',
+            'day_name' => 'Monday',
+            'active_week' => 'even',
+            'begins_at' => '10:00:00',
+            'ends_at' => '12:00:00',
+        ]);
+
+        Schedule::create([
+            'scheduled_from' => '2021-01-01',
+            'day_name' => 'Wednesday',
+            'active_week' => 'odd',
+            'begins_at' => '12:00:00',
+            'ends_at' => '16:00:00',
+        ]);
+
+        Schedule::create([
+            'scheduled_from' => '2021-01-01',
+            'day_name' => 'Friday',
+            'active_week' => 'all',
+            'begins_at' => '10:00:00',
+            'ends_at' => '16:00:00',
+        ]);
+
+        Schedule::create([
             'scheduled_from' => '2021-06-01',
             'scheduled_till' => '2021-11-30',
             'day_name' => 'Thursday',
