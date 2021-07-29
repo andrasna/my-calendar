33464ac87c2c5be8e68ddde9e633251a4de3faf9
Andras Nagy <contact@andras.me>
2021-07-29 14:56:41 +0200

    remove unused code

--- routes/api.php
+++ routes/api.php
@@ -20,10 +20,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::get('/schedules', function () {
-//     return Schedule::all();
-// });
-
 Route::get('/schedules', function () {
     return ScheduleResource::collection(Schedule::all());
 });
