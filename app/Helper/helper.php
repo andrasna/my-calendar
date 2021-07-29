88e348aebd93366e1ccc62622294aa2e62114b1b
Andras Nagy <contact@andras.me>
2021-07-29 20:13:04 +0200

    fix indentation

--- app/Helper/helper.php
+++ app/Helper/helper.php
@@ -4,7 +4,7 @@ namespace App\Helper;
 
 class Helper
 {
-  public static function strToDayAsNum($str) {
-    return intval(date('w', strtotime($str)));
-  }
+    public static function strToDayAsNum($str) {
+        return intval(date('w', strtotime($str)));
+    }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,22 +5,22 @@ import timeGridPlugin from '@fullcalendar/timegrid'
 const calendarEl = document.querySelector('.calendar-js')
 
 const calendar = new Calendar(calendarEl, {
-  plugins: [
-    dayGridPlugin,
-    timeGridPlugin,
-  ],
-  headerToolbar: {
-    left: 'prev,next today',
-    center: 'title',
-    right: 'timeGridDay,timeGridWeek,dayGridMonth'
-  },
-  weekends: false,
-  eventColor: 'rgba(44, 62, 80, .65)',
-  initialView: 'timeGridWeek',
-  navLinks: true, // can click day/week names to navigate views
-  editable: true,
-  dayMaxEvents: true, // allow "more" link when too many events
-  events: 'http://my-calendar.test/api/schedules',
+    plugins: [
+        dayGridPlugin,
+        timeGridPlugin,
+    ],
+    headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'timeGridDay,timeGridWeek,dayGridMonth'
+    },
+    weekends: false,
+    eventColor: 'rgba(44, 62, 80, .65)',
+    initialView: 'timeGridWeek',
+    navLinks: true, // can click day/week names to navigate views
+    editable: true,
+    dayMaxEvents: true, // allow "more" link when too many events
+    events: 'http://my-calendar.test/api/schedules',
 })
 
 calendar.render()
