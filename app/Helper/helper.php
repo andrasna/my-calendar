da90e6df171d5a29d87c94cf781575bb3f9c9904
Andras Nagy <contact@andras.me>
2021-07-30 13:16:35 +0200

    add helper functions to date

--- app/Helper/helper.php
+++ app/Helper/helper.php
@@ -1,10 +1,48 @@
 <?php
 
 namespace App\Helper;
+use DateTime;
 
 class Helper
 {
-    public static function strToDayAsNum($str) {
-        return intval(date('w', strtotime($str)));
+    public static function getTimeDiff($time1, $time2) {
+        return (new DateTime($time1))
+            ->diff(new DateTime($time2))
+            ->format('%H:%I:%S');
+    }
+
+    public static function isOddWeek($date) {
+        return date("W", strtotime($date)) % 2 === 1;
+    }
+
+    public static function isEvenWeek($date) {
+        return date("W", strtotime($date)) % 2 === 0;
+    }
+
+    public static function shiftWeek($date) {
+        return (new DateTime($date))->modify('+1 week')->format('Y-m-d');
+    }
+
+    public static function shiftToOddOrEvenWeek($date, $weeklyRecurrence) {
+        switch ($weeklyRecurrence) {
+            case 'onOdd':
+                return self::isOddWeek($date)
+                    ? $date
+                    : self::shiftToOddOrEvenWeek(self::shiftWeek($date), $weeklyRecurrence);
+                break;
+            case 'onEven':
+                return self::isEvenWeek($date)
+                    ? $date
+                    : self::shiftToOddOrEvenWeek(self::shiftWeek($date), $weeklyRecurrence);
+                break;
+            default:
+                return $date;
+        }
+    }
+
+    public static function getClosestDateOfAWeekday($weekday, $startingFrom) {
+        return (new DateTime($startingFrom))
+            ->modify('next ' . $weekday)
+            ->format('Y-m-d');
     }
 }
